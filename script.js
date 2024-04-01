ad90fd4af2d16f25780f0fb1305f982376b6274d
Emmanuel <emmanuel@MacBook-Pro-de-Patricia.local>
2024-04-01 02:13:48 -0600

    init

---
+++ script.js
@@ -0,0 +1,21 @@
+const models = document.getElementsByClassName("models");
+const moreBtn = document.getElementById("more-button");
+//console.log(models);
+const just = document.getElementById("justification");
+const paths = ["./assets/all-folded.glb", "./assets/big.glb", "./assets/small.glb", "./assets/dark.glb", "./assets/rugged.glb"];
+
+if(window.innerWidth < 400) {
+  console.log('small');
+  just.style.maxHeight = "180";
+  just.style.overflow = "hidden";
+  just.style.transition = "max-height .3s ease-in"
+}
+function toggleContent() {
+  just.style.maxHeight = "none";
+}
+
+moreBtn.addEventListener("click", toggleContent);
+
+console.log('hi');
+
+
